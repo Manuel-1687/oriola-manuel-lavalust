ba411acf55bba6e5e36012b8ebbf904c4410fe8f
Manuel-1687 <oriolapardz@gmail.com>
2026-08-20 13:14:05 +0800

    Restore welcome homepage

--- app/config/routes.php
+++ app/config/routes.php
@@ -3,6 +3,6 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 /** @var object $router **/
 
-$router->get('/', 'Home::index');
+$router->get('/', 'Welcome::index');
 $router->get('/home', 'Home::index');
 $router->get('/profile', 'Home::profile');
